431433f37a84afc2336b4217a56cf7aaebf156af
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-23 12:42:01 +0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -10,3 +10,29 @@ function greetings(name = 'Peter') {
   
   console.log(greetings())
   console.log(greetings('Asabeneh'))
+
+  a= "JavaScript"
+  console.log(a)
+
+  const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    getFullName: function() {
+      return `${this.firstName}${this.lastName}`
+    },
+    'phone number': '+3584545454545'
+  }
+  console.log(person.skills[1])
