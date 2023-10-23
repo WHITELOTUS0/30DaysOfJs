a1e1b9be4826e6da6624ba7f7ac236bae57680c2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-23 10:45:11 +0300

    Update introduction.js

--- introduction.js
+++ introduction.js
@@ -3,3 +3,6 @@ console.log(PI)
 console.log(Math.round(PI))
 let num = [3,5,0,-9,-13,6,7];
 console.log(Math.min(...num))
+console.log(Math.floor(Math.random()*11))
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log(string.match('love'))
