3b7ae6ecdc0941774f067069ed6756f1c37e629e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-23 12:21:04 +0300

    Updates

--- Arrays.js
+++ Arrays.js
@@ -3,3 +3,10 @@ function myFunction(){
 }
 
 myFunction(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+function greetings(name = 'Peter') {
+    let message = `${name}, welcome to 30 Days Of JavaScript!`
+    return message
+  }
+  
+  console.log(greetings())
+  console.log(greetings('Asabeneh'))
