6171c9bf60ec2148db8f18ddec7106a95d5f2dea
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-23 10:52:17 +0300

    Update introduction.js

--- introduction.js
+++ introduction.js
@@ -1,10 +1,19 @@
 const PI = Math.PI;
 console.log(PI)
 console.log(Math.round(PI))
-let num = [3,5,0,-9,-13,6,7];
+let num = [3, 5, 0, -9, -13, 6, 7];
 console.log(Math.min(...num))
-console.log(Math.floor(Math.random()*11))
+console.log(Math.floor(Math.random() * 11))
 let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
 console.log(string.match('love'))
-let pattern= '/love/gi'
+let pattern = /love/gi
 console.log(string.match(pattern))
+let txt = 'In 2019, I ran 30 Days of Python. Now, in 2020 I am super exited to start this challenge'
+let regEx = /\d+/
+
+// d with escape character means d not a normal d instead acts a digit
+// + means one or more digit numbers,
+// if there is g after that it means global, search everywhere.
+
+//console.log(txt.match(regEx))  // ["2", "0", "1", "9", "3", "0", "2", "0", "2", "0"]
+console.log(txt.match(/\d+/g)) // ["2019", "30", "2020"]
